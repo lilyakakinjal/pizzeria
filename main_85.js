5d62ad1a5804525163b6dcd8a793fc37359af002
Kinjal Dahiya <100461323+lilyakakinjal@users.noreply.github.com>
2022-06-19 09:17:06 +0530

    Add files via upload

---
+++ main_85.js
@@ -0,0 +1,19 @@
+menu_list_array = [ "Veg Margherita Pizza",                  
+ "Chicken Tandoori Pizza", "Veg Supreme Pizza", "Panner Tikka Pizza", "Deluxe Veggie Pizza", "Veg Extravaganza Pizza"];
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0; i<menu_list_array.length; i++)
+    {
+        htmldata=htmldata+ menu_list_array[i] + '<br>'
+    }
+     
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+function add_suggestion()
+{
+    
+	var item = document.getElementById("add_item").value;
+    menu_list_array.push(item);
+}
+
